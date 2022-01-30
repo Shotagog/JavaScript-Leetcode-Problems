33dff1ee3d4b3ede3926d810d9796a5e08fd9b3d
shotagog <muzaradi@gmail.com>
2022-01-30 16:21:26 +0400

    Contains Duplicate II

---
+++ containsDuplicateII.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////Contains Duplicate II/////////////////////////////////////////////
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such
+// that nums[i] == nums[j] and abs(i - j) <= k.
+// Example 1:
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+ const containsNearbyDuplicate = function(nums, k) {
+    for (let i = 0; i < nums.length; i++){
+        for (let j = i + 1; j < nums.length; j++){
+            if(nums[i] === nums[j] && Math.abs(i - j) <= k){
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+// console.log(containsNearbyDuplicate([1,2,3,1], 3));
+// console.log(containsNearbyDuplicate([1,0,1,1], 1));
+// console.log(containsNearbyDuplicate([1,2,3,1,2,3], 2));
